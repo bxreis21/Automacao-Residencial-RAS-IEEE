6921b02705ae97f5c4db52648f8ca1fc8293d2ed
Bruno Reis <bxreis21@gmail.com>
2023-04-02 14:59:24 -0300

    Sistema de Login e Register feitos e funcionando.
    Sistema de Login e Register feitos nas views do app 'Home', com algumas principais validações. Agora é possível criar e logar usuários no próprio site, sem a necessidade de entrar na área administrativa do django. Só está faltando colocar o Django Messages para aparecer possíveis erros e notificações para o usuário do site.

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.core.validators import validate_email
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from .models import Room
 
 def home(request):
@@ -7,8 +10,65 @@ def home(request):
         return render(request, 'home.html', {'rooms': rooms})
     return render(request, 'home.html')
 
-def login(request):
-    return render(request, 'login.html')
+def login_view(request):
+    if request.method != 'POST':
+        return render(request, 'login.html')
+    
+    username : str = request.POST.get('user')
+    password : str = request.POST.get('password')
 
-def register(request):
-    return render(request, 'register.html')
+    user = authenticate(request,username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return redirect('home')
+    
+    else:
+        return render(request, 'login.html')
+    
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def register_view(request):
+    if request.method != 'POST':
+        return render(request, 'register.html')
+    
+    data = {
+        'first_name': request.POST.get('name'),
+        'last_name' : request.POST.get('lastname'),
+        'username'  : request.POST.get('username'),
+        'email'     : request.POST.get('email'),
+        'password'  : request.POST.get('password'),
+        'password2' : request.POST.get('password2'),
+    }
+
+    for info in data.values():
+        if info is None:
+            return render(request, 'register.html')
+        
+    try:
+        validate_email(data['email'])
+    
+    except:
+        return render(request,'register.html')
+    
+    if User.objects.filter(username=data['username']).exists():
+        return render(request, 'register.html')
+    
+    elif User.objects.filter(email=data['email']).exists():
+        return render(request, 'register.html')
+
+    elif data['password'] != data['password2']:
+        return render(request, 'register.html')
+
+    user = User.objects.create_user(
+        username=data['username'],
+        password=data['password'],
+        email=data['email'],
+        first_name=data['first_name'],
+        last_name=data['last_name'],
+    )
+
+    user.save()
+    return redirect('home')
